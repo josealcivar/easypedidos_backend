9b552277e8dd9e392e6eabac5bda4015a15d437c
JoseAndre <JoseAndre@DESKTOP-CFGHQSK.(none)>
2019-09-18 17:41:58 -0500

    correciones de formato

--- app.js
+++ app.js
@@ -5,27 +5,11 @@ var logger = require('morgan');
 
 require('dotenv').config();
 
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
-
 var app = express();
-
-// var empresaRouter = require('./routes/routes_api_empresa');
-// var clienteRouter = require('./routes/routes_api_cliente');
-// var empleadoRouter = require('./routes/routes_api_empleados');
-// var pedidosRouter = require('./routes/routes_api_pedidos');
-// var gruposRouter = require('./routes/routes_api_grupos');
-// var productosRouter= require('./routes/routes_api_productos');
-// var marcaRouter= require('./routes/routes_api_marca');
-// var caracteristicasRouter= require('./routes/routes_api_caracteristicas');
-// var promocionesRouter= require('./routes/routes_api_promociones');
 
 var bodyParser = require('body-parser');
 
-
 require("./cors")(app);
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,18 +17,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+/**
+ * @description llama a todas las rutas una sola vez
+ */
 require("./routes/routes_api_all")(app);
-
-// app.use('/api/empresas', empresaRouter);
-// app.use('/api/clientes', clienteRouter);
-// app.use('/api/empleados', empleadoRouter);
-// app.use('/api/pedidos', pedidosRouter);
-// app.use('/api/grupos',gruposRouter);
-// app.use('/api/productos',productosRouter);
-// app.use('/api/marcas',marcaRouter);
-// app.use('/api/caracteristicas', caracteristicasRouter);
-// app.use('/api/promociones', promocionesRouter);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/routes_api_all.js
+++ routes/routes_api_all.js
@@ -1,27 +1,27 @@
 'use strict';
 
-
 module.exports = function(app) {
-    //  apps.use(cors());
-var empresaRouter = require('./routes_api_empresa');
-var clienteRouter = require('./routes_api_cliente');
-var empleadoRouter = require('./routes_api_empleados');
-var pedidosRouter = require('./routes_api_pedidos');
-var gruposRouter = require('./routes_api_grupos');
-var productosRouter= require('./routes_api_productos');
-var marcaRouter= require('./routes_api_marca');
-var caracteristicasRouter= require('./routes_api_caracteristicas');
-var promocionesRouter= require('./routes_api_promociones');
+    
+var empresaRouter         = require('./routes_api_empresa');
+var clienteRouter         = require('./routes_api_cliente');
+var empleadoRouter        = require('./routes_api_empleados');
+var pedidosRouter         = require('./routes_api_pedidos');
+var gruposRouter          = require('./routes_api_grupos');
+var productosRouter       = require('./routes_api_productos');
+var marcaRouter           = require('./routes_api_marca');
+var caracteristicasRouter = require('./routes_api_caracteristicas');
+var promocionesRouter     = require('./routes_api_promociones');
 
-app.use('/api/empresas', empresaRouter);
-app.use('/api/clientes', clienteRouter);
-app.use('/api/empleados', empleadoRouter);
-app.use('/api/pedidos', pedidosRouter);
-app.use('/api/grupos',gruposRouter);
-app.use('/api/productos',productosRouter);
-app.use('/api/marcas',marcaRouter);
+app.use('/api/empresas',        empresaRouter);
+app.use('/api/clientes',        clienteRouter);
+app.use('/api/empleados',       empleadoRouter);
+app.use('/api/pedidos',         pedidosRouter);
+app.use('/api/grupos',          gruposRouter);
+app.use('/api/productos',       productosRouter);
+app.use('/api/marcas',          marcaRouter);
 app.use('/api/caracteristicas', caracteristicasRouter);
-app.use('/api/promociones', promocionesRouter);
-  };
+app.use('/api/promociones',     promocionesRouter);
+
+};
   
 
